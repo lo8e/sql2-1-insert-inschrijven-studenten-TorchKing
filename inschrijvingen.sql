50a3276b45e620249e6e3fedfc170b0ac4aace79
IvanHolwerda <47853549+IvanHolwerda@users.noreply.github.com>
2019-05-08 09:39:45 +0200

    klassen

---
+++ inschrijvingen.sql
@@ -0,0 +1,6 @@
+USE `lo8e_sql2`;
+
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code) VALUES 
+("lo9e-amo1", "rs001", "2019", "amo"),
+("lo9e-amo2", "vlt03", "2019", "amo"),
+("lo9e-amo3", "bkr02", "2019", "amo");
