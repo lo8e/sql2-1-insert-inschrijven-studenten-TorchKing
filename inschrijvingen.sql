bdb295a1d85543d211896bd9bbbcd58644cb69cd
IvanHolwerda <47853549+IvanHolwerda@users.noreply.github.com>
2019-05-10 11:58:52 +0200

    studenten klassen

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -11,3 +11,8 @@ INSERT INTO studenten (ov_nummer, achternaam, tussenvoegsel, voornaam, geboorted
 ("96921", "hanzen", "van", "joost", "2019-06-09", "6667XC", "monster", "21", "1.666", "01-01-1900"),
 ("96669", "kees", "van", "kees", "1959-05-23", "2169XD", "friesland", "6.9", "1.30", "01-01-1960");
 
+INSERT INTO klassen_studenten (ov_nummer, klas_code) VALUES
+("96969", "lo9e-amo1"),
+("94202", "lo9e-amo2"),
+("96921", "lo9e-amo3"),
+("96669", "lo9e-amo");
